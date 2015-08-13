7f3916fb9acd662feb8e9b0f969432b058cd6849
Semyen Gabyshev <semyen.gabyshev@noveogroup.com>
2015-08-13 15:41:06 +0600

    tasks count validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
+  TASKS_COUNT = 4
+
   has_many :tasks
   has_many :people
   belongs_to :owner, :class_name => 'Person'
@@ -11,4 +13,18 @@ class Project < ActiveRecord::Base
   accepts_nested_attributes_for :owner, :reject_if => :all_blank
   accepts_nested_attributes_for :tags
   accepts_nested_attributes_for :project_tags, :allow_destroy => true
+
+  validate :tasks_count
+
+  private
+
+  def tasks_count
+    if existing_tasks.size != TASKS_COUNT
+      errors.add :base, message: :should_have_4_tasks
+    end
+  end
+
+  def existing_tasks
+    tasks.reject(&:marked_for_destruction?)
+  end
 end
